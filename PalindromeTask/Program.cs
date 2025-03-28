c9d50560b2f8112133da89c357855e82c03cb3ab
shaneaflyz <shaneaflyz@gmail.com>
2025-03-28 16:26:21 +0800

    feat: add palindrome function
    - scans three times though
    - if want scan once we can use left , right pointers
    - idk how to do that :P

--- PalindromeTask/Program.cs
+++ PalindromeTask/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿static bool IsPalindrome(string inputString, string trashSymbols)
+{
+  IEnumerable<char> filtered = inputString
+     .Where(c => !trashSymbols.Contains(c))  // Remove trash symbols
+     .Select(char.ToLower);
+
+  return filtered.SequenceEqual(filtered.Reverse());
+}
+
+Console.WriteLine(IsPalindrome("!!aacbb$aa", "$!"));
