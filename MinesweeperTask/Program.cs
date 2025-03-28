3173a7f95f35fd2a7080c106bb485ffe84484b62
shaneaflyz <shaneaflyz@gmail.com>
2025-03-28 17:21:22 +0800

    chore: sample grid
    - m*n grid where x is the bomb

--- MinesweeperTask/Program.cs
+++ MinesweeperTask/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+List<List<char>> grid = [
+  ['x','0','0','x','x'],
+  ['0','0','0','0','0'],
+  ['x','0','x','x','0'],
+  ['0','0','0','0','x'],
+  ['x','x','0','x','x'],
+  ['x','0','0','x','0'],
+];
